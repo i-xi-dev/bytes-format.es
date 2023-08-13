502ff335e8b3ef216836867882bd514d309b0c78
i-xi-dev <naoyuki.isono@gmail.com>
2023-08-14 05:47:01 +0900

    1.0.14

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export {
   Integer,
   type uint8,
-} from "https://raw.githubusercontent.com/i-xi-dev/int.es/1.3.2/mod.ts";
-export { StringUtils } from "https://raw.githubusercontent.com/i-xi-dev/str.es/2.0.0/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/int.es/1.4.1/mod.ts";
+export { StringUtils } from "https://raw.githubusercontent.com/i-xi-dev/str.es/2.0.1/mod.ts";
