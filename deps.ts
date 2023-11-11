231fe4b22efdfce84d8a1767a31be84524f2b43d
i-xi-dev <naoyuki.isono@gmail.com>
2023-11-11 22:39:19 +0900

    1.0.20

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export {
   PositiveInteger,
   Uint8,
-} from "https://raw.githubusercontent.com/i-xi-dev/number.es/3.0.2/mod.ts";
-export { StringUtils } from "https://raw.githubusercontent.com/i-xi-dev/str.es/2.0.7/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/number.es/3.0.4/mod.ts";
+export { StringUtils } from "https://raw.githubusercontent.com/i-xi-dev/str.es/2.0.8/mod.ts";
