1500573612181b0c663a2c8c1fdf033a63c423f3
i-xi-dev <naoyuki.isono@gmail.com>
2024-01-28 16:20:51 +0900

    1.0.28

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export {
   SafeInteger,
   Uint8,
-} from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.0.0/mod.ts";
-export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/6.0.2/mod.ts";
+} from "https://raw.githubusercontent.com/i-xi-dev/number.es/7.1.0/mod.ts";
+export { StringEx } from "https://raw.githubusercontent.com/i-xi-dev/string.es/6.0.3/mod.ts";
